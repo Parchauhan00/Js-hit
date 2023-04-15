62f9b363d3c8e5297edb5318a77ac038f3ad2295
Prashant Chauhan <vipchauhan00@gmail.com>
2023-04-15 09:52:43 +0000

    datatypes

---
+++ basic/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict";  // treat all JS code as newer version
+
+   // alert(3+3)         // we are using nodejs, not browser
+
+   console.log(3+3)
+                              // code readability should be high
+   console.log("Prime ch")
+
+   let name = "prashant"
+
+   let age = 20
+
+   let isLoggedIn = false
+
+   let tempreture = null;
+
+   // number => 2 to power 53
+   // bigint 
+   // String => " "
+   //boolean => true/false
+   // null => standalone value , 
+   // undefined => , when declare varaible  but not assigned value
+   //Symbol => unique
+
+
+   // object 
+
+   console.log(typeof "Prash");
+   console.log(typeof age);
+
+   console.log(typeof undefined);
+
+   console.log(typeof null);
+
+
