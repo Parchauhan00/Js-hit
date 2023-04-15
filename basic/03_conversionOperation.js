a4e5cfe1821cf80fbedc42c532f255cb969a7046
Prashant Chauhan <vipchauhan00@gmail.com>
2023-04-15 10:30:51 +0000

    conversion of DataType

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33aab"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "331bc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "prashant"
+// 1 => true; 0 => false
+// "" = >false
+// "hitesh" => true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// conversion in string
+let someNumber = 35
+
+let stringnumber = String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
